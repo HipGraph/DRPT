e687d6f14542349c804fcef0c78e6bfbfc387632
Isuru Ranawaka <irjanith@gmail.com>
2022-08-27 23:24:47 -0400

    drpt_global

--- cpp/src/dmrpt/algo/drpt_global.cpp
+++ cpp/src/dmrpt/algo/drpt_global.cpp
@@ -160,9 +160,11 @@ dmrpt::DRPTGlobal::grow_global_subtree(vector <vector<DataPoint>> &child_data_tr
         int no_of_bins = 1 + (3.322 * log2(data_vec_size));
 
 
+        cout<<" calculating mean"<<endl;
         VALUE_TYPE *result = mathOp.distributed_median(data, data_vec_size, 1,
                                                        total_size_vector[split_starting_index + i],
-                                                       28, dmrpt::StorageFormat::RAW, this->rank);
+                                                       no_of_bins, dmrpt::StorageFormat::RAW, this->rank);
+        cout<<" completed mean"<<endl;
 
         VALUE_TYPE median = result[0];
 

6b8d8fed77acc9952c976d95df1aaef6cd101a6e
Isuru Ranawaka <irjanith@gmail.com>
2022-08-28 14:13:45 -0400

    drpt_global

--- cpp/src/dmrpt/algo/mdrpt.hpp
+++ cpp/src/dmrpt/algo/mdrpt.hpp
@@ -41,7 +41,7 @@ namespace dmrpt {
 
         void grow_trees(float density);
 
-        void calculate_nns(map<int, vector<dmrpt::DataPoint> > &local_nns,int tree, int nn);
+        void calculate_nns(map<int, vector<dmrpt::DataPoint> > const &local_nns,int tree, int nn);
 
         vector <vector<dmrpt::DataPoint>> gather_nns(int nn);
 

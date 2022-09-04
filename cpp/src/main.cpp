ca641a3eeecf518c13d27a1a39953bd995aac509
Isuru Ranawaka <irjanith@gmail.com>
2022-09-04 19:23:51 -0400

    correlation count

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -184,7 +184,7 @@ int main(int argc, char *argv[]) {
 //        }
 //    }
 
-    fout << rank << ' ' << io_time.count() << ' ' << duration_index_building.count() << ' ' << duration_query.count()
-         << endl;
+//    fout << rank << ' ' << io_time.count() << ' ' << duration_index_building.count() << ' ' << duration_query.count()
+//         << endl;
     MPI_Finalize();
 }

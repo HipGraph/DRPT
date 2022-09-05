3f5970c61163061c13246bac1c916283b0afcea7
Isuru Ranawaka <irjanith@gmail.com>
2022-09-04 21:38:40 -0400

    correlation count

--- cpp/src/dmrpt/algo/drpt_global.cpp
+++ cpp/src/dmrpt/algo/drpt_global.cpp
@@ -512,12 +512,13 @@ dmrpt::DRPTGlobal::calculate_tree_leaf_correlation() {
                 int size = data_points.size();
                 std::transform(correlation_matrix[tree][leaf][c].begin(), correlation_matrix[tree][leaf][c].end(),
                                correlation_matrix[tree][leaf][c].begin(), [&](float x){(x/size)*100;});
-                float* max_element = std::max_element(correlation_matrix[tree][leaf][c].begin(),
-                                                     correlation_matrix[tree][leaf][c].end());
-                int selected_leaf = max_element - correlation_matrix[tree][leaf][c].begin();
+                int selected_leaf = std::max_element(correlation_matrix[tree][leaf][c].begin(),
+                                                     correlation_matrix[tree][leaf][c].end()) - correlation_matrix[tree][leaf][c].begin()
+                float max_element = *std::max_element(correlation_matrix[tree][leaf][c].begin(),
+                                                     correlation_matrix[tree][leaf][c].end())
 
                 final_mapping[tree][leaf][c] = selected_leaf;
-                    fout << selected_leaf << ' - '<<(*max_element)<<' ';
+                    fout << selected_leaf << ' - '<<(max_element)<<' ';
 
             }
             fout <<endl;

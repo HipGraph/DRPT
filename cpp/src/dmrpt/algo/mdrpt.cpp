b81bdd234ffb32d451d2e9b2b506dd8c208f4db5
Isuru Ranawaka <irjanith@gmail.com>
2022-09-09 10:02:17 -0500

    correlation count

--- cpp/src/dmrpt/algo/mdrpt.cpp
+++ cpp/src/dmrpt/algo/mdrpt.cpp
@@ -297,8 +297,8 @@ void dmrpt::MDRPT::calculate_nns(map<int, vector<dmrpt::DataPoint>> &local_nns, 
             } else {
                 std::vector<dmrpt::DataPoint> dst;
                 auto it = local_nns.find(idx);
-//                std::merge(it->second.begin(), it->second.end(), sub_vec.begin(),
-//                           sub_vec.end(), std::back_inserter(dst));
+                std::merge(it->second.begin(), it->second.end(), sub_vec.begin(),
+                           sub_vec.end(), std::back_inserter(dst));
 //                dst.erase(unique(dst.begin(), dst.end(),
 //                                 [](const DataPoint &lhs,
 //                                    const DataPoint &rhs) {

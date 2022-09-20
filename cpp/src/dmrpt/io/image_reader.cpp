a0e88c9de4bd20066529b0927361dc38a86f6960
Isuru Ranawaka <irjanith@gmail.com>
2022-09-19 22:29:19 -0400

    drpt global

--- cpp/src/dmrpt/io/image_reader.cpp
+++ cpp/src/dmrpt/io/image_reader.cpp
@@ -225,7 +225,9 @@ dmrpt::ImageReader::mpi_file_read(string path, int rank, int world_size, int ove
 
     cout << "rank" << rank << " null character assigned completed " << perpsize << endl;
 
-    cout << "rank" << rank << " chunk ##### " << chunk << endl;
+    if(rank==0) {
+        cout << "rank" << rank << " chunk ##### " << chunk << endl;
+    }
     //move to next full delim of number
     if (rank != world_size - 1) {
         while (chunk[locend] != delim)

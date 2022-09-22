cc3c6362b33882475a723bc7b9be2a34b606a55d
Isuru Ranawaka <irjanith@gmail.com>
2022-09-21 23:30:22 -0400

    mpi file read

--- cpp/src/dmrpt/io/image_reader.cpp
+++ cpp/src/dmrpt/io/image_reader.cpp
@@ -206,7 +206,7 @@ dmrpt::ImageReader::mpi_file_read(string path, int rank, int world_size, int ove
     //read relevant chunk
 //    int  error  = MPI_File_get_size(in, &filesize);
     filesize = 17550888879;
-    if(error != MPI_SUCCESS) cout << " cannot get file size " << endl;;
+//    if(error != MPI_SUCCESS) cout << " cannot get file size " << endl;;
 //    filesize--;
 
     perpsize = (filesize) / world_size;

defbdad75dcb87fdbb32133aaea849a4ee20ea2c
Isuru Ranawaka <irjanith@gmail.com>
2022-09-28 16:20:23 -0400

    reduce query  communication

--- cpp/src/dmrpt/algo/mdrpt.hpp
+++ cpp/src/dmrpt/algo/mdrpt.hpp
@@ -39,7 +39,7 @@ namespace dmrpt {
 
     private:
         std::map<int, vector<dmrpt::DataPoint>>
-        communicate_nns(std::map<int, vector<dmrpt::DataPoint> > &local_nns,vector<int> &keys, int nn);
+        communicate_nns(std::map<int, vector<dmrpt::DataPoint> > &local_nns,set<int> &keys, int nn);
 
         void calculate_nns(std::map<int, vector<dmrpt::DataPoint> > &local_nns, set<int> &keys, int tree, int nn);
 

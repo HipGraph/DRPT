da8613bf703557ec2cc8d08311992411abaa1829
Isuru Ranawaka <irjanith@gmail.com>
2022-09-28 12:21:43 -0400

    reduce query  communication

--- cpp/src/dmrpt/algo/drpt_global.cpp
+++ cpp/src/dmrpt/algo/drpt_global.cpp
@@ -820,12 +820,13 @@ void dmrpt::DRPTGlobal::calculate_tree_leaf_correlation (string outpath)
 //      sortByFreq (vec, candidate_mapping[j][k][m], this->world_size);
 //    }
 
-#pragma omp parallel for collapse(3)
+
   for (int j = 0; j < this->ntrees; j++) {
+     #pragma omp parallel for
       for (int k = 0; k < total_leaf_size; k++) {
-//          final_tree_leaf_mapping[k] = vector<int>(this->ntrees, -1);
+          final_tree_leaf_mapping[k] = vector<int>(this->ntrees, -1);
           for (int m = 0; m < this->ntrees; m++) {
-//              candidate_mapping[j][k][m] = vector<PriorityMap>(total_leaf_size);
+              candidate_mapping[j][k][m] = vector<PriorityMap>(total_leaf_size);
 
 //                for (int n = 0; n < total_leaf_size; n++) {
 //                    PriorityMap priorityMap;

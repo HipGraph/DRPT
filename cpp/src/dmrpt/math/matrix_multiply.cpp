09cee45c60346501a38ae449cdc72ccfed48177b
Isuru Ranawaka <irjanith@gmail.com>
2022-10-01 13:58:49 -0400

    reduce query  communication

--- cpp/src/dmrpt/math/matrix_multiply.cpp
+++ cpp/src/dmrpt/math/matrix_multiply.cpp
@@ -222,10 +222,10 @@ dmrpt::MathOp::distributed_median(vector<VALUE_TYPE> &data, vector<int> local_ro
                                   dmrpt::StorageFormat format, int rank) {
 //    cout<<" rank "<<rank<<" distributed median started "<<endl;
     VALUE_TYPE *means = this->distributed_mean(data, local_rows, local_cols, total_elements_per_col, format, rank);
-//    cout<<" rank "<<rank<<" distributed mean completed "<<means[0]<<endl;
+/    cout<<" rank "<<rank<<" distributed mean completed "<<means[0]<<endl;
     VALUE_TYPE *variance = this->distributed_variance(data, local_rows, local_cols, total_elements_per_col, format,
                                                       rank);
-//    cout<<" rank "<<rank<<" distributed variance completed "<<variance[0]<<endl;
+    cout<<" rank "<<rank<<" distributed variance completed "<<variance[0]<<endl;
     VALUE_TYPE *medians = (VALUE_TYPE *) malloc(sizeof(VALUE_TYPE) * local_cols);
 
     int std1 = 4, std2 = 2, std3 = 1;

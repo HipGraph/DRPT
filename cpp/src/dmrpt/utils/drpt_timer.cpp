28853e218648436f40354f9848e10a1858c0f66b
Isuru Ranawaka <irjanith@gmail.com>
2022-12-22 11:57:28 -0500

    fix compilation issues

--- cpp/src/dmrpt/utils/drpt_timer.cpp
+++ cpp/src/dmrpt/utils/drpt_timer.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 using namespace std::chrono;
 
-auto dmrpt::Timer::get_timelapse_in_milliseconds()
+auto dmrpt::Timer<auto>::get_timelapse_in_milliseconds()
 {
   return this->timelapse;
 }
 
- auto dmrpt::Timer::get_timelapse_in_seconds()
+ auto dmrpt::Timer<auto>::get_timelapse_in_seconds()
 {
   return this->timelapse/1000;
 }
 
-void dmrpt::Timer::record_start_time()
+void dmrpt::Timer<auto>::record_start_time()
 {
   this->start_timestamp = high_resolution_clock::now();
 }
 
-void dmrpt::Timer::record_end_time ()
+void dmrpt::Timer<auto>::record_end_time ()
 {
   this->stop_timestamp = high_resolution_clock::now();
   this->timelapse = duration_cast<microseconds> (this->stop_timestamp - this->start_timestamp);

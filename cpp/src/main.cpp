0d667b50362cc26c9611f3ec5d2e4e2908fff0bc
Isuru Ranawaka <irjanith@gmail.com>
2023-01-21 17:24:52 -0500

    mpi edge list writer

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -223,7 +223,7 @@ int main(int argc, char* argv[])
 
 	auto file_writing_started = high_resolution_clock::now();
 	FileWriter<int> fileWriter;
-//	fileWriter.mpi_write_edge_list(data_points,file_path_results,nn-1,rank,size,true);
+	fileWriter.mpi_write_edge_list(data_points,file_path_results,nn-1,rank,size,true);
 
 	cout <<"rank "<<rank<< " file writing end " << endl;
 

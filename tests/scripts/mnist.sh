a5e9e9edd4a902914d1de34eaeee30616f070853
Isuru Ranawaka <irjanith@gmail.com>
2023-03-13 11:11:21 -0400

    Add README

--- tests/scripts/mnist.sh
+++ tests/scripts/mnist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --qos=debug
 #SBATCH --time=30:00
-#SBATCH --nodes=1
-#SBATCH --tasks=4
+#SBATCH --nodes=4
+#SBATCH --tasks=16
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=8
 #SBATCH --constraint=haswell
@@ -12,4 +12,4 @@ export OMP_PROC_BIND=true
 export OMP_PLACES=threads
 export OMP_NUM_THREADS=8
 
-srun -n 4  distributed_mrpt -input distributed-mrpt/tests/datasets/train-images-idx3-ubyte.gz train-images-idx3-ubyte -output ./ -data-set-size 60000 -dimension 784  -ntrees 8  -nn 10  -locality 1 -file_format 0
+srun -n 16  drpt -input drpt/tests/datasets/train-images-idx3-ubyte.gz train-images-idx3-ubyte -output ./ -data-set-size 60000 -dimension 784  -ntrees 8  -nn 10  -locality 1 -file_format 0
